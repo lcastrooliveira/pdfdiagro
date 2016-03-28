913dd3d3f12952706d5eb0ba917cdcbdd729694a
Your Name <allowski@gmail.com>
2016-03-28 14:57:21 -0400

    Commit Message:
    TODO:
    
    Check the Todos in app.js
    
    1- Implement a callback system for the downloadFile function
    2- Write a recursive function for the downloadFile function
    3- Check and test every function

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,21 @@ function onDeviceReady() {
     });
 };
 
-function downloadFile() {
+// TODO:
+
+// use properly the errorHandle variable
+
+// Implement forceOverwrite parameter
+
+// Separate the downloadFile function from openFile function
+
+// Call it downloadFile and openPdf respectively
+
+function downloadFile(URL, successCallback, openFileAfterDownload){
+
+    // Set default value for openFileAfterDownload to False (So, if I want to open it after download set it to true when call the function)
+
+    openFileAfterDownload = openFleAfterDownload || false;
 
     var fileTransfer = new FileTransfer();
     var uri = encodeURI("http://www.fab.mil.br/cabine/concursos/IE_EA_EAOEAR_2017.pdf");
@@ -34,6 +48,10 @@ function downloadFile() {
 
     // Save location
     var fileURL = cordova.file.externalDataDirectory + filename;
+
+    // TODO:
+
+    // -- check if the file exists before start the download
 
     fileTransfer.download(
         uri,
@@ -48,6 +66,16 @@ function downloadFile() {
                         console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
                     },
                     success : function () {
+
+			// call the successCallback 
+
+			if(typeof(successCallback) === "function"){
+				
+				// finally call the valid callback
+				successCallback(fileURL);
+
+			}
+
                         console.log('file opened successfully');
                     }
                 }
@@ -64,3 +92,37 @@ function downloadFile() {
     );
 }
 
+
+// TODO: Create a recursive function example
+
+// downloadPdfArray([URL1, URL2, URL3], function(pdfCount){ alert("Job completed, downloaded "+pdfCount);  });
+
+function downloadPdfArray(URLArray, successCallback){
+
+	var i = 0;
+
+	var pdfCount = URLArray.length;
+
+	// Recursive function
+
+	function downloadNextPdf(){
+
+		if(i > pdfCount){
+
+			// call the successCallback on complete
+
+			successCallback(pdfCount);
+
+			return;
+
+		}
+
+		downloadFile(URLArray[i], function(){
+
+			downloadNextPdf();		
+	
+		});
+	
+	}
+
+}
