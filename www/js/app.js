848a2deb68385b33c86a977c8d7db66e133e5de1
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2016-04-05 01:47:17 -0300

    Finishd Recursive function

--- www/js/app.js
+++ www/js/app.js
@@ -20,40 +20,51 @@ $(document).ready(function() {
 function onDeviceReady() {
     $('.acao-abrir').on('click',function() {
         console.log("Cliquei no botao");
-        //downloadFile("http://www.fab.mil.br/cabine/concursos/IE_EA_EAOEAR_2017.pdf",null,false,true);
-        var f = new FileDownloader("http://www.fab.mil.br/cabine/concursos/IE_EA_EAOEAR_2017.pdf",null,false,true);
-        f.startDownload();
+        //Instanciar o objeto FileDownloader e setar os parametros
+        var f = new FileDownloader(function(m) {console.log(m);},true,false);
+        var array = ["http://www.fab.mil.br/cabine/concursos/IE_EA_EAOEAR_2017.pdf","http://www.fab.mil.br/cabine/concursos/01_IE_EA_CFOINF_2017.pdf","http://www.fab.mil.br/cabine/concursos/01_IE_EA_CFOINT_2017.pdf"];
+        //Passar o array para fazer os downloads
+        f.downloadPdfArray(array)
+        //f.startDownload();
     });
 };
 
-function FileDownloader(URL, successCallback, failCallback, openFileAfterDownload, forceOverwrite) {
-    var URL = URL;
+function FileDownloader(successCallback,openFileAfterDownload, forceOverwrite) {
     var successCallback = successCallback;
-    var failCallback = failCallback;
     var openFileAfterDownload = openFileAfterDownload || false;
     var forceOverwrite = forceOverwrite || false;
-    var fileTransfer = new FileTransfer();
-    var uri = encodeURI(URL);
-    // File name only
-    var filename = uri.split("/").pop();
-    // Save location
-    var fileURL = cordova.file.externalDataDirectory + filename;
 
-    this.startDownload = function() {
-        window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, checkDir,
-            function() {console.log("fail resolve");});
+    var fileTransfer = new FileTransfer();
+    console.log("start download Array");
+    this.downloadPdfArray = function (URLArray) {
+
+        if(URLArray.length == 0 ) {
+            return;
+        }
+        var url = URLArray.shift();
+        this.startDownload(url);
+        this.downloadPdfArray(URLArray);
     };
 
-    var checkDir = function (directoryEntry) {
+    this.startDownload = function(url) {
+        var uri = encodeURI(url);
+        window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(directoryEntry) {
+            checkDir(directoryEntry,uri);
+        }, function() {console.log("fail resolve");});
+    };
+
+    var checkDir = function (directoryEntry,uri) {
         if(forceOverwrite) {
-            executeDownload();
+            var filename = uri.split("/").pop();
+            executeDownload(uri,filename);
         } else {
             var directoryReader = directoryEntry.createReader();
-            directoryReader.readEntries(getNewName,function() {console.log("fail entry()");})
+            directoryReader.readEntries(function(entries) {getNewName(entries,uri);},function() {console.log("fail entry()");})
         }
     };
 
-    var getNewName = function (entries) {
+    var getNewName = function (entries,uri) {
+        var filename = uri.split("/").pop();
         while(true) {
             var changed = false;
             var i;
@@ -78,18 +89,21 @@ function FileDownloader(URL, successCallback, failCallback, openFileAfterDownloa
             }
         }
         console.log(filename);
-        fileURL = cordova.file.externalDataDirectory + filename;
-        executeDownload();
+        executeDownload(uri,filename);
         //file:///storage/emulated/0/Android/data/py.com.diagro/files/IE_EA_EAOEAR_2017.pdf
     };
 
-    var executeDownload = function() {
+    var executeDownload = function(uri,filename) {
+        var fileURL = cordova.file.externalDataDirectory + filename;
         fileTransfer.download(
             uri,
             fileURL,
-            //successCallback(fileURL),
-            function() {
-                console.log('success');
+            function(entry) {
+                successCallback("download complete: "+entry.toURL());
+                //console.log('success');
+                if(openFileAfterDownload) {
+                    openPdf(entry);
+                }
             },
             function(error) {
                 console.log("download error source " + error.source);
@@ -101,118 +115,21 @@ function FileDownloader(URL, successCallback, failCallback, openFileAfterDownloa
         );
     };
 
-}
+    var openPdf = function (path) {
 
-// TODO:
-
-// use properly the errorHandle variable
-
-// Implement forceOverwrite parameter
-
-// Separate the downloadFile function from openFile function
-
-// Call it downloadFile and openPdf respectively
-
-function downloadFile(URL, successCallback, openFileAfterDownload, forceOverwrite){
-
-    // Set default value for openFileAfterDownload to False (So, if I want to open it after download set it to true when call the function)
-
-    openFileAfterDownload = openFileAfterDownload || false;
-    forceOverwrite = forceOverwrite || true;
-
-    var fileTransfer = new FileTransfer();
-    var uri = encodeURI(URL);
-
-
-    // File name only
-    var filename = uri.split("/").pop();
-
-
-    // Save location
-    var fileURL = cordova.file.externalDataDirectory + filename;
-    // TODO:
-
-    // -- check if the file exists before start the download
-    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, logFileName, function() {console.log("fail resolve");});
-    /*
-    fileTransfer.download(
-        uri,
-        fileURL,
-        //successCallback(fileURL),
-        function() {
-            console.log('success');
-        },
-        function(error) {
-            console.log("download error source " + error.source);
-            console.log("download error target " + error.target);
-            console.log("download error code" + error.code);
-        },
-        true,
-        {}
-    );
-    */
-}
-
-function logFileName(directoryEntry) {
-    console.log(forceOverwrite);
-    var directoryReader = directoryEntry.createReader();
-    directoryReader.readEntries(logEntries,function() {console.log("fail entry()");})
-}
-
-function logEntries(entries) {
-    var i;
-    for (i=0; i<entries.length; i++) {
-        console.log(entries[i].name);
-    }
-}
-
-// TODO: Create a recursive function example
-
-// downloadPdfArray([URL1, URL2, URL3], function(pdfCount){ alert("Job completed, downloaded "+pdfCount);  });
-
-function downloadPdfArray(URLArray, successCallback){
-
-	var i = 0;
-
-	var pdfCount = URLArray.length;
-
-	// Recursive function
-
-	function downloadNextPdf(){
-
-		if(i > pdfCount){
-
-			// call the successCallback on complete
-
-			successCallback(pdfCount);
-
-			return;
-
-		}
-
-		downloadFile(URLArray[i], function(){
-
-			downloadNextPdf();		
-	
-		});
-	
-	}
-
-}
-
-function openPdf(path) {
-
-    cordova.plugins.fileOpener2.open(
-        path.toURL(),
-        'application/pdf',
-        {
-            error : function(e) {
-                console.log('error: '+e);
-            },
-            success : function() {
-                console.log('file opened successfully');
+        cordova.plugins.fileOpener2.open(
+            path.toURL(),
+            'application/pdf',
+            {
+                error : function(e) {
+                    console.log('error: '+e);
+                },
+                success : function() {
+                    console.log('file opened successfully');
+                }
             }
-        }
-    );
+        );
 
-}
+    }
+
+};
